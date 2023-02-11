8aa33a11296a2e851cc98362894c2088bd363203
JPJAP <51898962+porajap@users.noreply.github.com>
2023-02-11 18:25:04 +0700

    add querry foramt

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -28,14 +28,13 @@ export function validateToken(req, res, next) {
   }
 
   const token = authHeader.split(" ")[1];
+
   if (token == null) {
     res.sendStatus(400).send("Token not present");
     return;
   }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(user);
-
     if (err) {
       res.status(403).send("Token invalid");
     } else {
